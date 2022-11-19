2d11da083c8942be556fa17541314253b2b994df
karan8404 <114310500+karan8404@users.noreply.github.com>
2022-11-19 19:30:08 +0530

    .

--- DSA/sorts/HeapSort.java
+++ DSA/sorts/HeapSort.java
@@ -1,6 +1,77 @@
 package sorts;
-import Heaputils.Heap;
+
+import java.util.ArrayList;
+
+// Java program for implementation of Heap Sort
 
 public class HeapSort {
-    
+    public static void sort(int arr[]) {
+        int N = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+
+        // extracting an element from heap
+        for (int i = N - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    static void heapify(int arr[], int N, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, N, largest);
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static String toString(int arr[]) {
+        int N = arr.length;
+        StringBuilder res=new StringBuilder();
+
+        for (int i = 0; i < N; ++i)
+            res.append(arr[i]+" ");
+        res.append("\r\n");
+
+        return res.toString();
+    }
+
+    // Driver's code
+    public static void main(String args[]) {
+        ArrayList<Integer> a=Input.input();
+        int[] arr=new int[a.size()];
+        for(int i=0;i<a.size();i++){
+            arr[i]=a.get(i);
+        }
+
+        sort(arr);
+
+        System.out.println(toString(arr));
+    }
 }
