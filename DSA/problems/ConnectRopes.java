a889fa1d94356af5b32562eaa54fe7b57d6119d7
karan8404 <114310500+karan8404@users.noreply.github.com>
2022-11-23 23:31:43 +0530

    .

--- DSA/problems/ConnectRopes.java
+++ DSA/problems/ConnectRopes.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 import sorts.Input;
 
 //Find the minimum cost of connecting ropes. Video #34
-class ConnectRopes{
+class ConnectRopes {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        ArrayList<Integer> list=Input.inputAlist(sc);
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = Input.inputAlist(sc);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.addAll(list);
 
-        int sum=0;
+        int sum = 0;
         int temp;
-        while(pq.size()>1){
-            temp=pq.poll()+pq.poll();
-            sum+=temp;
+        while (pq.size() > 1) {
+            temp = pq.poll() + pq.poll();
+            sum += temp;
             pq.add(temp);
         }
         System.out.println(sum);
--- DSA/problems/StreamMedian.java
+++ DSA/problems/StreamMedian.java
@@ -1,3 +1,47 @@
 //program to find the median at any instant while the input is being provided in a stream.
+//input number to put into stream, x to stop the program, any other character to find the median.
 
 import java.util.*;
+
+class StreamMedian {
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());// maxHeap
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();// minHeap
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size())// if both have same size, return average of elements.
+            return (maxHeap.peek() + minHeap.peek()) / 2;
+        else// max heap has more elements return root element.
+            return maxHeap.peek();
+    }
+
+    public void put(int a) {
+        maxHeap.add(a);
+        if (maxHeap.size() - minHeap.size() >= 2)// if max has 2 more elements than min then put one into min.
+            minHeap.add(maxHeap.poll());
+        if (maxHeap.size() != 0 && minHeap.size() != 0) {
+            if (maxHeap.peek() > minHeap.peek()) {// if top element in max is greater than top element in min, swap.
+                int temp = maxHeap.poll();
+                maxHeap.add(minHeap.poll());
+                minHeap.add(temp);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        StreamMedian ob = new StreamMedian();
+        Scanner sc = new Scanner(System.in);
+        char ch;
+
+        while (true) {
+            ch = sc.next().charAt(0);
+            if (Character.isDigit(ch))// if character is number call put().
+                ob.put(Integer.valueOf(ch));
+            else if (ch == 'x') {// if x is enter find the median then exit.
+                System.out.println(ob.findMedian());
+                break;
+            } else// else find the current median.
+                System.out.println(ob.findMedian());
+        }
+        sc.close();
+    }
+}
