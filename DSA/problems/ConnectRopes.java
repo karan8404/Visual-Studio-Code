921e448e193a81aa852248621a18da26b53ddc44
karan8404 <114310500+karan8404@users.noreply.github.com>
2022-11-23 21:54:31 +0530

    created ConnectRopes

--- DSA/problems/ConnectRopes.java
+++ DSA/problems/ConnectRopes.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import sorts.Input;
+
 //Find the minimum cost of connecting ropes. Video #34
 class ConnectRopes{
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> list=Input.inputAlist(sc);
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        pq.addAll(list);
+
+        int sum=0;
+        int temp;
+        while(pq.size()>1){
+            temp=pq.poll()+pq.poll();
+            sum+=temp;
+            pq.add(temp);
+        }
+        System.out.println(sum);
+    }
 }
