f747de4692b2810b8a579cfb002d77151a92cf21
karan8404 <114310500+karan8404@users.noreply.github.com>
2023-04-17 18:44:22 +0530

    Created bitboard to efficiently store board informtion also storing last move made.

--- projects/Tic_Tac_Toe/BitBoard.java
+++ projects/Tic_Tac_Toe/BitBoard.java
@@ -1,15 +1,78 @@
 package Tic_Tac_Toe;
 
 public class BitBoard {
-    
-    public static byte[][] makeBoard(char[][] board){
-        byte[][] bitBoard=new byte[3][3];
+    public Pos[] board;
+    public boolean player;
+    public int lastLoc;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                
-            }
+    public BitBoard() {
+        board = new Pos[9];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = new Pos();
         }
-        return null;
+        player=true;
+        lastLoc=-1;
+    }
+
+    public Pos pos(int loc) {
+        return board[loc];
+    }
+
+    public void displayBoard() {
+        System.out.printf("%c|%c|%c\n-|-|-\n", board[0].getMark(), board[1].getMark(), board[2].getMark());
+        System.out.printf("%c|%c|%c\n-|-|-\n", board[3].getMark(), board[4].getMark(), board[5].getMark());
+        System.out.printf("%c|%c|%c\n", board[6].getMark(), board[7].getMark(), board[8].getMark());
+    }
+
+    public boolean makeMove(int loc) {
+        if (board[loc].isEmpty()) {
+            board[loc].setMark(player);
+            player=!player;
+            lastLoc=loc;
+            return true;
+        }
+        else{
+            System.out.println("Invalid Move!");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        BitBoard board = new BitBoard();
+
+        board.makeMove(1);
+        board.makeMove(5);
+        board.makeMove(7);
+
+        board.displayBoard();
+    }
+
+    public static class Pos {
+        boolean hasMark;
+        boolean mark;// true='X',false='O'
+
+        public Pos() {
+            hasMark = false;
+        }
+
+        boolean isEmpty() {
+            return !hasMark;
+        }
+
+        char getMark() {
+            if (isEmpty())
+                return ' ';
+            return (mark ? 'X' : 'O');
+        }
+
+        void setMark(boolean player) {
+            // if player set 'X' else set 'O'
+            mark = player;
+            hasMark=true;
+        }
+
+        void clearMark() {
+            hasMark = false;
+        }
     }
 }
